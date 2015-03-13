68defbd3990f87b13ca23a9103240c079c2cd95c
Bastian Bechtold <basti@bastibe.de>
2015-03-13 09:33:45 +0100

    moved cdef into pysoundfile_binaries package
    this makes soundfile.py much easier to read

--- soundfile.py
+++ soundfile.py
@@ -12,121 +12,9 @@ __version__ = "0.6.0"
 
 import numpy as _np
 import os as _os
-from cffi import FFI as _FFI
 from os import SEEK_SET, SEEK_CUR, SEEK_END
+from pysoundfile_binaries import snd as _snd, ffi as _ffi
 
-_ffi = _FFI()
-_ffi.cdef("""
-enum
-{
-    SF_FORMAT_SUBMASK       = 0x0000FFFF,
-    SF_FORMAT_TYPEMASK      = 0x0FFF0000,
-    SF_FORMAT_ENDMASK       = 0x30000000
-} ;
-
-enum
-{
-    SFC_GET_FORMAT_INFO             = 0x1028,
-
-    SFC_GET_FORMAT_MAJOR_COUNT      = 0x1030,
-    SFC_GET_FORMAT_MAJOR            = 0x1031,
-    SFC_GET_FORMAT_SUBTYPE_COUNT    = 0x1032,
-    SFC_GET_FORMAT_SUBTYPE          = 0x1033,
-    SFC_SET_CLIPPING                = 0x10C0,
-} ;
-
-enum
-{
-    SF_FALSE    = 0,
-    SF_TRUE     = 1,
-
-    /* Modes for opening files. */
-    SFM_READ    = 0x10,
-    SFM_WRITE   = 0x20,
-    SFM_RDWR    = 0x30,
-} ;
-
-typedef int64_t sf_count_t ;
-
-typedef struct SNDFILE_tag SNDFILE ;
-
-typedef struct SF_INFO
-{
-    sf_count_t frames ;        /* Used to be called samples.  Changed to avoid confusion. */
-    int        samplerate ;
-    int        channels ;
-    int        format ;
-    int        sections ;
-    int        seekable ;
-} SF_INFO ;
-
-SNDFILE*    sf_open          (const char *path, int mode, SF_INFO *sfinfo) ;
-int         sf_format_check  (const SF_INFO *info) ;
-
-sf_count_t  sf_seek          (SNDFILE *sndfile, sf_count_t frames, int whence) ;
-
-int         sf_command       (SNDFILE *sndfile, int cmd, void *data, int datasize) ;
-
-int         sf_error         (SNDFILE *sndfile) ;
-const char* sf_strerror      (SNDFILE *sndfile) ;
-const char* sf_error_number  (int errnum) ;
-
-int         sf_perror        (SNDFILE *sndfile) ;
-int         sf_error_str     (SNDFILE *sndfile, char* str, size_t len) ;
-
-int         sf_close         (SNDFILE *sndfile) ;
-void        sf_write_sync    (SNDFILE *sndfile) ;
-
-sf_count_t  sf_read_short    (SNDFILE *sndfile, short *ptr, sf_count_t items) ;
-sf_count_t  sf_read_int      (SNDFILE *sndfile, int *ptr, sf_count_t items) ;
-sf_count_t  sf_read_float    (SNDFILE *sndfile, float *ptr, sf_count_t items) ;
-sf_count_t  sf_read_double   (SNDFILE *sndfile, double *ptr, sf_count_t items) ;
-
-sf_count_t  sf_readf_short   (SNDFILE *sndfile, short *ptr, sf_count_t frames) ;
-sf_count_t  sf_readf_int     (SNDFILE *sndfile, int *ptr, sf_count_t frames) ;
-sf_count_t  sf_readf_float   (SNDFILE *sndfile, float *ptr, sf_count_t frames) ;
-sf_count_t  sf_readf_double  (SNDFILE *sndfile, double *ptr, sf_count_t frames) ;
-
-sf_count_t  sf_write_short   (SNDFILE *sndfile, short *ptr, sf_count_t items) ;
-sf_count_t  sf_write_int     (SNDFILE *sndfile, int *ptr, sf_count_t items) ;
-sf_count_t  sf_write_float   (SNDFILE *sndfile, float *ptr, sf_count_t items) ;
-sf_count_t  sf_write_double  (SNDFILE *sndfile, double *ptr, sf_count_t items) ;
-
-sf_count_t  sf_writef_short  (SNDFILE *sndfile, short *ptr, sf_count_t frames) ;
-sf_count_t  sf_writef_int    (SNDFILE *sndfile, int *ptr, sf_count_t frames) ;
-sf_count_t  sf_writef_float  (SNDFILE *sndfile, float *ptr, sf_count_t frames) ;
-sf_count_t  sf_writef_double (SNDFILE *sndfile, double *ptr, sf_count_t frames) ;
-
-sf_count_t  sf_read_raw      (SNDFILE *sndfile, void *ptr, sf_count_t bytes) ;
-sf_count_t  sf_write_raw     (SNDFILE *sndfile, void *ptr, sf_count_t bytes) ;
-
-const char* sf_get_string    (SNDFILE *sndfile, int str_type) ;
-int         sf_set_string    (SNDFILE *sndfile, int str_type, const char* str) ;
-
-typedef sf_count_t  (*sf_vio_get_filelen) (void *user_data) ;
-typedef sf_count_t  (*sf_vio_seek)        (sf_count_t offset, int whence, void *user_data) ;
-typedef sf_count_t  (*sf_vio_read)        (void *ptr, sf_count_t count, void *user_data) ;
-typedef sf_count_t  (*sf_vio_write)       (const void *ptr, sf_count_t count, void *user_data) ;
-typedef sf_count_t  (*sf_vio_tell)        (void *user_data) ;
-
-typedef struct SF_VIRTUAL_IO
-{    sf_count_t  (*get_filelen) (void *user_data) ;
-     sf_count_t  (*seek)        (sf_count_t offset, int whence, void *user_data) ;
-     sf_count_t  (*read)        (void *ptr, sf_count_t count, void *user_data) ;
-     sf_count_t  (*write)       (const void *ptr, sf_count_t count, void *user_data) ;
-     sf_count_t  (*tell)        (void *user_data) ;
-} SF_VIRTUAL_IO ;
-
-SNDFILE*    sf_open_virtual   (SF_VIRTUAL_IO *sfvirtual, int mode, SF_INFO *sfinfo, void *user_data) ;
-SNDFILE*    sf_open_fd        (int fd, int mode, SF_INFO *sfinfo, int close_desc) ;
-
-typedef struct SF_FORMAT_INFO
-{
-    int         format ;
-    const char* name ;
-    const char* extension ;
-} SF_FORMAT_INFO ;
-""")
 
 _str_types = {
     'title':       0x01,
@@ -231,28 +119,13 @@ _default_subtypes = {
     'RF64':  'PCM_16',
 }
 
+
 _ffi_types = {
     _np.dtype('float64'): 'double',
     _np.dtype('float32'): 'float',
     _np.dtype('int32'): 'int',
     _np.dtype('int16'): 'short'
 }
-
-
-try:
-    _snd = _ffi.dlopen('sndfile')
-except OSError as err:
-    from sys import platform
-    from platform import architecture
-
-    if platform == 'win32' and architecture()[0] == '32bit':
-        _snd = _ffi.dlopen('pysoundfile_binaries/win32/libsndfile')
-    elif platform == 'win32' and architecture()[0] == '64bit':
-        _snd = _ffi.dlopen('pysoundfile_binaries/win64/libsndfile')
-    elif platform == 'darwin':
-        _snd = _ffi.dlopen('pysoundfile_binaries/darwin/libsndfile')
-    else:
-        raise err
 
 
 def read(file, frames=-1, start=0, stop=None, dtype='float64', always_2d=True,
